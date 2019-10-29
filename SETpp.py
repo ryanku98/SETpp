ae4735a6d23b813656b09634e78c32459d61e36e
evan <eejohnson@scu.edu>
2019-10-29 16:33:45 -0700

    added sqlalchemy to requirements

--- SETpp.py
+++ SETpp.py
@@ -1,1 +1,14 @@
-from app import app
+from app import app,db
+from app.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+  return {'db': db, 'User': User}
+
+def add_user(username, email):
+  new_user = User(username=username, email=email)
+  db.session.add(new_user)
+  db.session.commit()
+
+add_user('Admin', 'admin@scu.edu')
+print(User.query.all())
