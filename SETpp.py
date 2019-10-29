0c0f5350d4cc80d9286021b103ba9b2024a4a72a
evan <eejohnson@scu.edu>
2019-10-29 16:59:11 -0700

    non relational database

--- SETpp.py
+++ SETpp.py
@@ -5,10 +5,20 @@ from app.models import User
 def make_shell_context():
   return {'db': db, 'User': User}
 
-def add_user(username, email):
-  new_user = User(username=username, email=email)
-  db.session.add(new_user)
-  db.session.commit()
+def add_user(username_, email_, password_):
+  if not User.query.filter_by(username='Admin').first():
+    new_user = User(username=username_, email=email_, password_hash=password_)
+    db.session.add(new_user)
+    db.session.commit()
 
-add_user('Admin', 'admin@scu.edu')
+def get_email(username_):
+  user = User.query.filter_by(username=username_)
+  return user.first().email
+
+def hash_pass(password):
+  return password
+
+User.query.delete()
+add_user('Admin', 'admin@scu.edu', 'password')
 print(User.query.all())
+print(get_email('Admin'))
