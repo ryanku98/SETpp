d1180d4496fbe8e9445da5f9f1f5bfb7d1e8b1f7
Ryan Ku <ryanku98@gmail.com>
2019-10-29 16:54:01 -0700

    Create survey form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, SurveyForm
 
 @app.route('/')
 @app.route('/index')
@@ -11,11 +11,14 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for admin {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for admin {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
     
-@app.route('/survey')
+@app.route('/survey', methods=['GET', 'POST'])
 def survey():
-    return render_template('survey.html')
+    form = SurveyForm()
+    if form.validate_on_submit():
+        flash('Survey submission requested for student {}, course {}'.format(form.student_id.data, form.course_id.data))
+        return redirect(url_for('survey'))
+    return render_template('survey.html', form=form)
