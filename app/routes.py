92622cef2ee90ae0a2c089f05ac7f4f2fe9eb28a
Ryan Ku <ryanku98@gmail.com>
2019-10-31 13:35:38 -0700

    @evan comment

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # @EVAN put login stuff here
         flash('Login requested for admin {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
