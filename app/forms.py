a2aeaecf1fad21370cd0f286c5658f765f7db8c3
Ryan Ku <ryanku98@gmail.com>
2019-10-31 13:35:13 -0700

    Add registration form

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, RadioField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
+    def validate_empty_userbase(self):
+        if User.query.count() != 0:
+            raise ValidationError('An admin already exists for this system.')
 
 class SurveyForm(FlaskForm):
     # SUBMISSION VALIDATION
