0a1e453c44bae290a4c8739ef30cf3b411891e15
Ryan Ku <ryanku98@gmail.com>
2019-10-31 13:10:36 -0700

    Set global variable roster.csv

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+roster_filename = "roster.csv"
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'put-secondary-key-here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-coen174-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')       #Flask-SQLAlchemy extension takes location of app's database from SQLALCHEMY_DATABASE_URI config variable
     SQLALCHEMY_TRACK_MODIFICATIONS = False
