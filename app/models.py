4a42bb7251d8e3f885ded5c532e166d4153959c3
Ryan Ku <ryanku98@gmail.com>
2019-10-31 16:55:06 -0700

    Remove code with Posts

--- app/models.py
+++ app/models.py
@@ -5,11 +5,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 # inherits from db.Model, a base class for all models in Flask-SQLAlchemy
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)      #defines fields as class variables, or instances of db.Column class, taking field type as an argument
+    # defines fields as class variables, or instances of db.Column class, taking field type as an argument
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    # posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -19,12 +19,3 @@ class User(UserMixin, db.Model):
         
     def check_password_hash(self, password):
         return check_password_hash(self.password_hash, password)
-
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.String(140))
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))       #references id value form user table
-
-#     def __repr__(self):
-#         return '<Post {}>'.format(self.body)
