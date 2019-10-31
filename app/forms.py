c6d9f7c4e7d1dd3b13fbc5def92c71841a6b9102
Ryan Ku <ryanku98@gmail.com>
2019-10-31 14:15:05 -0700

    Fix User import

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, RadioField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
