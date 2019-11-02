efa94ad29a5c4074958659d6714c6e9e363cbec3
Ryan Ku <ryanku98@gmail.com>
2019-11-01 21:47:50 -0700

    Delete unnecessary code

--- SETpp.py
+++ SETpp.py
@@ -1,6 +1,5 @@
 from app import app, db
 from app.models import User
-from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.shell_context_processor
 def make_shell_context():
