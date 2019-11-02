974ee5b20b87f3146985e9df87927dd3fe6cb40b
Ryan Ku <ryanku98@gmail.com>
2019-11-01 21:34:38 -0700

    Fix login bug, remove next_page stuff

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, login_required
-from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, SurveyForm
 from app.models import User
 from app.results import submitResult
+from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -12,6 +14,11 @@ def index():
     if not current_user.is_authenticated:
         flash('Login to view admin page!')
         return redirect(url_for('login'))
+    if form.validate_on_submit():
+        f_roster = form.roster.data
+        filename = secure_filename(f_roster.filename)
+        f.save(os.path.join('documents', roster.csv))
+        
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,16 +28,17 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         # @EVAN put login stuff here
-        user = User.query.filter_by(email=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+        # next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #     next_page = url_for('index')
         # flash('Login requested for admin {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(next_page)
+        # return redirect(next_page)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
     
 @app.route('/register', methods=['GET', 'POST'])
