633dc3b828c0d2bb0ae9b569dd97d042920492f0
Ryan Ku <ryanku98@gmail.com>
2019-11-01 17:50:21 -0700

    Delete repeated code

--- app/models.py
+++ app/models.py
@@ -19,9 +19,3 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-        
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-        
-    def check_password_hash(self, password):
-        return check_password_hash(self.password_hash, password)
