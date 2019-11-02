4857f4cc20c6944a108d30c394318a66c43c2114
Ryan Ku <ryanku98@gmail.com>
2019-11-01 17:33:47 -0700

    Initialize results.csv function

--- app/statistics.py
+++ app/statistics.py
@@ -1,29 +1,34 @@
+import os
 import csv
 import pandas as pd
 
-qs = ['The labs helped me understand the lecture material.', 'The labs taught me new skills.', 'The labs taught me to think creatively.', 'I would be able to repeat the labswithout help.', 'The lab instructor was supportive.', 'The lab instructor was approachable.', 'The lab instructor was able to answer my questions.', 'The lab instructor helped me reach a clear understanding of key concepts.', 'The lab instructor fostered a mutually respectful learning environment.', 'The amount of lab equipmentwas sufficient.', 'The available space was sufficient for the lab activities.', 'If lab equipment or setups were not functioning properly, adequate support was available to rectify the situation.'
+# qs = ['The labs helped me understand the lecture material.', 'The labs taught me new skills.', 'The labs taught me to think creatively.', 'I would be able to repeat the labswithout help.', 'The lab instructor was supportive.', 'The lab instructor was approachable.', 'The lab instructor was able to answer my questions.', 'The lab instructor helped me reach a clear understanding of key concepts.', 'The lab instructor fostered a mutually respectful learning environment.', 'The amount of lab equipmentwas sufficient.', 'The available space was sufficient for the lab activities.', 'If lab equipment or setups were not functioning properly, adequate support was available to rectify the situation.'
 
-df = pd.read_csv(r'stats.csv')
-# print(df)
+statistics_file = 'documents/results.csv'
+questions_file = 'documents/survey_questions.txt'
 
-means = []
-
-c = 0
-for (q in qs):
-    means[c]= df[qs[c]].mean()
-    c = c + 1
-    
-    
-for (mean in means):
-    print("Mean for " + qs[1] + " : " + str(mean1))
-
-mean4 = df[].mean()
-
-
-
-# FREE RESPONSE QUESTIONS
-
-#What was your favorite aspect of the lab?Free text response
-#What about the lab would you like to see improved?Free text response
-#What, if anything, could the lab instructor do to improve the experience?Free text response
-#What, if anything, could improve lab space and equipment?Free text response
+def initResultsTable():
+    if not os.path.exists(statistics_file):
+        with open(statistics_file, 'w') as f_statistics, open(questions_file, 'r') as f_questions:
+            csvfile = csv.writer(f_statistics, delimiter=',')
+            headers = f_questions.readlines()
+            # put course ID in first column
+            headers.insert(0, 'Class Nbr')
+            # put instructor email in second column
+            headers.insert(1, 'Instructor Email')
+            csvfile.writerow(headers)
+        print('Headers loaded')
+        
+# def andresfunction():
+#     total = len(mc_qs)
+# 
+#     df = pd.read_csv(r'../documents/stats.csv')
+#     # print(df)
+# 
+#     means = []
+# 
+#     for i in range(0, total):
+#         means.append(df[mc_qs[i]].mean())
+# 
+#     for i in range(0, total):
+#         print("Mean for \"" + mc_qs[i] + "\": " + str(means[i]))
