dd2bffba2691343f1d53b9a7abb7b5a21b00e18e
Ryan Ku <ryanku98@gmail.com>
2019-11-02 22:27:17 -0700

    Retrieve and sort results

--- app/results.py
+++ app/results.py
@@ -1,37 +1,64 @@
 import os
 import csv
-import pandas as pd
-from app.roster import roster_file, course_id_i, instructor_email_i
+# import pandas as pd
+# from app.roster import roster_file, course_id_i, instructor_email_i
+from roster import roster_file, course_id_i, instructor_email_i
 
 questions_file = os.path.join('documents', 'survey_questions.txt')
 results_file = os.path.join('documents', 'results.csv')
 statistics_file = os.path.join('documents', 'statistics.csv')
 
 # initialize results table if DNE
-def initResultsTable():
-    if not os.path.exists(results_file):
-        with open(results_file, 'w', newline='') as f_results, open(questions_file, 'r') as f_questions:
-            headers = f_questions.readlines()
-            # put instructor email in first column
-            headers.insert(0, 'Instructor Email')
-            # put course ID in second column
-            headers.insert(1, 'Class Nbr')
+def initTable(filepath):
+    # STANDARDIZE INIT
+    if not os.path.exists(filepath):
+        with open(filepath, 'w', newline='') as f_results:
             csv_results = csv.writer(f_results, delimiter=',')
-            csv_results.writerow(headers)
+            csv_results.writerow(getHeaders())
             print('Headers loaded')
+
+# retrieve headers: instructor email + class nbr + questions
+def getHeaders():
+    with open(questions_file, 'r') as f_questions:
+        headers = f_questions.readlines()
+        # put instructor email in first column
+        headers.insert(0, 'Instructor Email')
+        # put course ID in second column
+        headers.insert(1, 'Class Nbr')
+        return headers
 
 # enters new submission into results table    
 def submitResult(submission):
     # init table if DNE
     if not os.path.exists(results_file):
-        initResultsTable()
+        initTable(results_file)
     # get instructor email using course number, insert into front of list
     submission.insert(0, searchInstructorEmail(submission[0]))
     with open(results_file, 'a', newline='') as f_results:
         csv_results = csv.writer(f_results, delimiter=',')
         csv_results.writerow(submission)
         print('New submission inserted')
-        
+    
+# return results in sorted order
+def getSortedResults():
+    # reset statistics file
+    if os.path.exists(statistics_file):
+        os.remove(statistics_file)
+    initTable(statistics_file)
+    
+    # extract result entries and sort by course
+    with open(results_file, 'r', newline='') as f_results:
+        csv_results = csv.reader(f_results, delimiter=',')
+        # extract entries
+        entries = []
+        for row in csv_results:
+            entries.append(row)
+        # remove header row
+        entries.pop(0)
+        # sort by course (column index 1)
+        entries.sort(key=lambda entry: int(entry[1]))
+        return entries
+    
 def searchInstructorEmail(course_id):
     with open(roster_file, 'r', newline='') as f_roster:
         csv_roster = csv.reader(f_roster, delimiter=',')
