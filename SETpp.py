54f70a8db5736ec63a49a7f0e88953c2f3ec744a
evan <eejohnson@scu.edu>
2019-11-02 20:57:56 -0700

    check pull

--- SETpp.py
+++ SETpp.py
@@ -33,11 +33,7 @@ def register(User):
 
 # if len(User.query.all()) == 0:
 #   register(User)
-<<<<<<< HEAD
-  
-=======
 # 
->>>>>>> bf34d9b5e3b47819a8efb4c96e8024ab3fb47a0d
 # else:
 #   login(User)
 # print(get_email('Admin'))
