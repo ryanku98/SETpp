ff2680f59f00f868e8817d4d3cb318d84a9acdc6
andresjmorales <morales.andres98@gmail.com>
2019-11-03 16:05:21 -0800

    Added new email functions

--- app/emails2.py
+++ app/emails2.py
@@ -24,53 +24,40 @@ class Student:
         self.email = email
         self.course = course
 
+    def create_message(self):
+        body = "Hello " + self.name + ",\nPlease fill your lab evaluations for course #" + self.course + " out at the following link: google.com"
+        message = MIMEMultipart()
+        message["From"] = SENDER_EMAIL
+        message["To"] = str(self.email)
+        message["Subject"] = self.name + ", please fill out your lab evaluations"
+        message.attach(MIMEText(body, "plain"))
+        msg = message.as_string()
+        return msg
+
+    def send_email(self):
+        msg = self.create_message()
+        email(self.email, msg)
+
 
 class Professor:
     def __init__(self, email):
         self.email = email
 
 
-def get_profs(roster_file)
+def get_profs(roster_file):
     profs = []
     with open(roster_file, newline='') as csvfile:
+        next(csvfile)
         sr = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in sr:
             email = row[student_email_i]
             course = row[course_id_i]
             prof = Professor(email)
-            students.append(prof)
-    return students
-
-def make_students(roster_file):
-    students = []
-    with open(roster_file, newline='') as csvfile:
-        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in sr:
-            name = row[student_id_i]
-            email = row[student_email_i]
-            course = row[course_id_i]
-            student = Student(name, email, course)
-            students.append(student)
-    return students
+            profs.append(prof)
+    return profs
 
 
-def student_message(student):
-    body = "Hello " + student.name + ",\nPlease fill your lab evaluations for course #" + student.course + " out at the following link: google.com"
-    message = MIMEMultipart()
-    message["From"] = SENDER_EMAIL
-    message["To"] = str(student.email)
-    message["Subject"] = student.name + ", please fill out your lab evaluations"
-    message.attach(MIMEText(body, "plain"))
-    msg = message.as_string()
-    return msg
-
-
-def send_student_email(student):
-    msg = student_message(student)
-    send_email(student.email, msg)
-
-
-def send_email(email, msg):           # GENERIC SEND EMAIL
+def email(email, msg):           # GENERIC SEND EMAIL
     smtp_server = "smtp.gmail.com"
     port = 587
     context = ssl.create_default_context()
@@ -83,16 +70,22 @@ def send_email(email, msg):           # GENERIC SEND EMAIL
 
 
 def send_all_student_emails():
-    students = make_students(roster)
-    for student in students[1:]:
-        send_student_email(student)
+    with open(roster, newline='') as csvfile:
+        next(csvfile)
+        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in sr:
+            name = row[student_id_i]
+            email = row[student_email_i]
+            course = row[course_id_i]
+            student = Student(name, email, course)
+            student.send_email()
+            print("sent email to " + email)
 
 
 ########
 # MAIN #
 send_all_student_emails()
-
-send_all_prof_emails()
+# send_all_prof_emails()
 
 
 # prof emails (only used at end of deadline)
@@ -107,14 +100,4 @@ def make_prof_message(sender_email, student_email, name, courseid):
 
     return msg
 
-
-# Runs through roster, checks if student of matching student ID and course ID exists
-def studentExists(s_id, c_id):
-    with open(roster_file, 'r', newline='') as f_roster:
-        csv_roster = csv.reader(f_roster, delimiter=',')
-        for row in csv_roster:
-            if row[student_id_i] == str(s_id) and row[course_id_i] == str(c_id):
-                print('Student found')
-                return True
-    print('Student not found')
-    return False
+# end
