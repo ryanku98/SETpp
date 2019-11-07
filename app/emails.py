fd264904cd1917a36cafb0bebd3144c63899dd1d
Ryan Ku <ryanku98@gmail.com>
2019-11-06 18:37:00 -0800

    Refactor emails module (rm 1-time vars, imports, simplify, etc.)

--- app/emails.py
+++ app/emails.py
@@ -9,21 +9,7 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from threading import Thread
-from app.survey import student_id_i, course_id_i, prof_email_i, student_email_i
-
-
-# CONSTANTS
-SENDER_EMAIL = "setsystempp@gmail.com"; SENDER_PSWD = "setpp_coen174"
-SUBJECT = "SET++ Lab Evaluations"; LINK = "http://localhost:5000/survey"
-stud_msg = os.path.join('app', 'templates', 'email', 'studentSurveyLink.txt')
-prof_msg = os.path.join('app', 'templates', 'email', 'professorSurveyStatistics.txt')
-roster = os.path.join('documents', 'roster.csv')
-roster_headers = ['Term', 'Class Nbr', 'Subject', 'Catalog', 'Title', 'Section', 'Instructor', 'Instructor Email', 'Student ID', 'Student', 'Email', 'Tot Enrl', 'Unit Taken', 'Grade', 'Campus', 'Location', 'Add Dt', 'Drop Dt', 'Comb Sect', 'Career', 'Component', 'Session', 'Class Type', 'Grade Base']
-# student_id_i = 8
-# course_id_i = 1
-# prof_email_i = 7
-# student_email_i = 9
-
+from app.survey import student_id_i, course_id_i, prof_email_i, student_email_i, roster_file, results_file
 
 # STUDENT CLASS
 class Student:
@@ -34,23 +20,31 @@ class Student:
 
     def create_message(self):
         '''Creates an email message string based on the student'''
-        with open(stud_msg, 'r') as file:
+        stud_msg_template = os.path.join('app', 'templates', 'email', 'studentSurveyLink.txt')
+        with open(stud_msg_template, 'r') as file:
             body = file.read().format(self.name, self.course, LINK)
         message = MIMEMultipart()
         message["From"] = SENDER_EMAIL
         message["To"] = self.email
         message["Subject"] = "{} - {}".format(self.name, SUBJECT)
         message.attach(MIMEText(body, "plain"))
-        msg = message.as_string()
-        return msg
+        return message.as_string()
 
     # Function to call on a student to send an email to the student
     def send_email(self):
-        msg = self.create_message()
-        # msg = message(self.email, prof_msg, self.name, self.course)
-        # message() works but unnecessary
-        email(self.email, msg)
+        email(self.email, self.create_message())
 
+
+def send_all_student_emails():
+    '''This is the function that should be called when the survey is started'''
+    with open(roster_file, newline='') as csvfile:
+        next(csvfile)
+        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in sr:
+            if row[student_id_i] and row[student_email_i] and row[course_id_i]:
+                student = Student(name, email, course)
+                student.send_email()
+                print("Sent email to student {}".format(email))
 
 # PROFESSOR CLASS
 class Professor:
@@ -59,46 +53,40 @@ class Professor:
 
     def create_message(self):
         '''Will be pretty similar to one above, pull from the other email template'''
-        with open(prof_msg, 'r') as file:
+        prof_msg_template = os.path.join('app', 'templates', 'email', 'professorSurveyStatistics.txt')
+        with open(prof_msg_template, 'r') as file:
             body = file.read().format(self.email, LINK)
         message = MIMEMultipart()
         message["From"] = SENDER_EMAIL
         message["To"] = self.email
         message["Subject"] =  "Professor {} - {}".format(self.email, SUBJECT)
         message.attach(MIMEText(body, "plain"))
-        msg = message.as_string()
-        return msg
+        return message.as_string()
 
     def send_email(self):
-        msg = self.create_message()
-        email(self.email, msg)
+        email(self.email, self.create_message())
 
-
-'''
-def message(email, template, name, course):
-    with open(template, 'r') as file:
-        if (template == stud_msg):
-            body = file.read().format(name, course, LINK) # Student
-        else:
-            body = file.read().format(name, "STATS GO HERE") # Prof
-    message = MIMEMultipart()
-    message["From"] = SENDER_EMAIL
-    message["To"] = email
-    message["Subject"] = "{} - {}".format(name, SUBJECT)
-    message.attach(MIMEText(body, "plain"))
-    msg = message.as_string()
-    return msg
-'''
-
+def send_all_prof_emails():
+    '''Function to email all professors'''
+    with open(results_file, newline='') as csvfile:
+        next(csvfile)
+        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in sr:
+            if row[prof_email_i] and row[course_id_i]:
+                prof = Professor(name, email, course)
+                prof.send_email()
+                print("Sent email to professor {}".format(email))
 
 def email(email, msg):
     '''This is the generic SMTP emailing method (able to be used anywhere)'''
+    SENDER_EMAIL = "setsystempp@gmail.com"; SENDER_PSWD = "setpp_coen174"
+    SUBJECT = "SET++ Lab Evaluations"; LINK = "http://localhost:5000/survey"
     smtp_server = "smtp.gmail.com"
     port = 587
     context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as smtp:  #Opens connection with variable name "server"
-        smtp.ehlo()   #Identifies with mail server we are using
-        smtp.starttls(context=context)   #Start encrypting traffic
+    with smtplib.SMTP(smtp_server, port) as smtp:   # Opens connection with variable name "server"
+        smtp.ehlo() # Identifies with mail server we are using
+        smtp.starttls(context=context)  # Start encrypting traffic
         smtp.ehlo()
         smtp.login(SENDER_EMAIL, SENDER_PSWD)
         try:
@@ -106,10 +94,10 @@ def email(email, msg):
         except:
             print("Error: invalid email")
 
-
+# password reset email
 def send_password_reset_email(user):
     '''Evan's password reset function'''
-    token = user.get_reset_password_token()     #generate token for email
+    token = user.get_reset_password_token() # generate token for email
     body = "Hello, please follow the link to reset password: " + url_for('resetPassword', token=token, _external=True)
     message = MIMEMultipart()
     message["From"] = SENDER_EMAIL
@@ -118,42 +106,3 @@ def send_password_reset_email(user):
     message.attach(MIMEText(body, "plain"))
     msg = message.as_string()
     Thread( target = email, args = (message['To'], msg) ).start()
-
-
-def send_all_prof_emails():
-    '''Function to email all professors'''
-    with open(results, newline='') as csvfile:
-        next(csvfile)
-        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in sr:
-            email = row[prof_email_i]
-            course = row[course_id_i]
-            if not email or not course:
-                pass
-            else:
-                prof = Professor(name, email, course)
-                prof.send_email()
-                print("Sent email to professor {}".format(email))
-
-
-def send_all_student_emails():
-    '''This is the function that should be called when the survey is started'''
-    with open(roster, newline='') as csvfile:
-        next(csvfile)
-        sr = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in sr:
-            name = row[student_id_i]
-            email = row[student_email_i]
-            course = row[course_id_i]
-            if not name or not email or not course:
-                pass
-            else:
-                student = Student(name, email, course)
-                student.send_email()
-                # print("Sent email to student {}".format(email))
-
-
-# MAIN
-# send_all_student_emails()
-
-# end
