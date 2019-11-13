f1715fb8e79a6613fa685e17f82173b468903f01
Ryan Ku <ryanku98@gmail.com>
2019-11-12 23:09:01 -0800

    Only print scheduler log if emails are sent

--- app/__init__.py
+++ app/__init__.py
@@ -28,35 +28,33 @@ scheduler = BackgroundScheduler()
 def check_dates():
     """Check existing deadlines and reminders in the database, executes any corresponding action if one has passed"""
     now = datetime.utcnow()
-    print('-------------------------------------------------')
-    print('Checking deadlines at {}'.format(now))
 
+    d_sent = False
     deadline = Deadline.query.first()
     # if deadline exists, check if already passed
     if deadline is not None:
-        print('DEADLINE: {}'.format(deadline))
         if not is_valid_datetime(deadline.datetime, now):
             # send results and delete deadline
             send_all_prof_emails()
+            d_sent = True
             Deadline.query.delete()
-    else:
-        print('DEADLINE NOT FOUND')
 
     reminders = Reminder.query.all()
-    print('TIME NOW: {}'.format(now))
-    print('REMINDERS: {}'.format(reminders))
-    # sent flag: if a reminder has been sent already, other reminders that may have passed within the same interval should not trigger another reminder (and thus should be removed)
-    sent = False
+    # rem_sent flag: if a reminder has been sent already, other reminders that may have passed within the same interval should not trigger another reminder (and thus should be removed)
+    rem_sent = False
     for reminder in reminders:
         if not is_valid_datetime(reminder.datetime, now):
             # send emails only if a reminder has not been sent yet
-            if not sent:
+            if not rem_sent:
                 # TODO: CHANGE TO SEND REMINDER EMAIL
                 send_all_student_emails()
-                sent = True
+                rem_sent = True
             db.session.delete(reminder)
             print('Reminder {} sent.'.format(reminder))
     db.session.commit()
+
+    if d_sent or rem_sent:
+        print('------------------------------------\nTIME NOW: {}\nDEADLINE: {}\nREMINDERS: {}\n------------------------------------'.format(now, deadline, reminders))
 
 print('Scheduler starting...')
 scheduler.start()
