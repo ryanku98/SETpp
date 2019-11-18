674c08af72e847dacdf924458f537becad557fca
Ryan Ku <ryanku98@gmail.com>
2019-11-18 00:14:17 -0800

    Update with new database objects

--- SETpp.py
+++ SETpp.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User
+from app.models import User, Section, Student, Result, Deadline, Reminder
 
 @app.shell_context_processor
 def make_shell_context():
-  return {'db': db, 'User': User, 'Deadline': Deadline, 'Reminder': Reminder}
+  return {'db': db, 'User': User, 'Section': Section, 'Student': Student, 'Result': Result, 'Deadline': Deadline, 'Reminder': Reminder}
